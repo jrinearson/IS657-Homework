aa8a601c19def550f209ad806dd4d0f3c1945312
Justin <justin@rinearson.io>
2022-05-24 18:16:42 -0700

    Removing exclamation mark

--- HW1/App.js
+++ HW1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello IS 657 from Justin Rinearson!</Text>
+      <Text>Hello IS 657 from Justin Rinearson</Text>
       <StatusBar style="auto" />
     </View>
   );
